4397a460257016bdb2487530e05510421391c887
smwbtech <smwb.tech@gmail.com>
2017-11-09 23:58:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+let app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Index Page', messageHeader: 'Hello world!', messageText: 'This is my test express app'});
+});
+
+app.use(express.static(__dirname + '/public'));
+
+//Ошибка 404
+app.use( (req, res) => {
+    res.status(404);
+    res.render('index', {title: '404 Page', messageHeader: 'Ooops, it is 404 page!', messageText: 'This is my test express app 404 page'});
+});
+
+//Ошибка 500
+app.use( (err, req, res, next) => {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Ошибка сервера');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Exptress is runnin on http//localhost' + app.get('port') + '; Press Cntl+C for ending!');
+});
